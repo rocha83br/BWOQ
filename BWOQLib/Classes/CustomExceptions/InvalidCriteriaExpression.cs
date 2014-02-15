a2c7e4113f646a0b9a66d1de3a321dc485cf1cef
Renato Rocha <rocha83br@gmail.com>
2014-02-15 02:51:13 -0300

    Predicate Query Validation

---
+++ BWOQLib/Classes/CustomExceptions/InvalidCriteriaExpression.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Linq.Dynamic.BitWise
+{
+    public class InvalidCriteriaExpression : Exception
+    {
+        internal const string errDesc = "Invalid criteria expression. Try : Numeric[AttribCombin]:Alphanumeric[SearchTerm]&[Consider Conjuntion]=[Consider Equality]";
+        public InvalidCriteriaExpression() : base(errDesc)
+        {
+        }
+    }
+}
---
+++ BWOQLib/Classes/CustomExceptions/InvalidQueryExpression.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Linq.Dynamic.BitWise
+{
+    public class InvalidQueryExpression : Exception
+    {
+        internal const string errDesc = "Invalid query expression. Try : Numeric[AttribCombin]>Numeric[ChildOrdinal]:Numeric[ChildAttribCombin]";
+        public InvalidQueryExpression() : base(errDesc)
+        {
+        }
+    }
+}
